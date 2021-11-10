4e1989ffc173399089daf05ab7e87c72cfca83e8
Michael Towson <MTowson@gmail.com>
2021-11-10 11:46:16 -0800

    finish events component

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,7 +1,55 @@
+import Link from 'next/link';
+
 function EventItem(props) {
+  //Destructure props
+  const { title, image, date, location, id } = props;
+
+  //--- FUNCTIONS ---
+  
+  //Function for making date data more readable. Pass in "date" from props
+  const humanReadableDate = new Date(date).toLocaleDateString("en-us", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  //Function for making address location more readable.
+  //Replaces any comma and space with a line break ('\n'). Location comes from props.
+  const formattedLocation = location.replace(", ", "\n");
+
+  //Function for putting props id into a functioning url link
+  const exploreLink = `/events/${id}`;
+
+
+
+
+  //--- COMPONENT RENDER ---
   return (
-    moo
-  )
+    <li>
+      {/* Serve image from props */}
+      <img src={'/' + image} alt={title} /> 
+      
+      <div>
+        {/* Name */}
+        <div>
+          <div>
+            <h2>{title}</h2>
+          </div>
+          <div>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div>
+            <address>{formattedLocation}</address>
+          </div>
+        </div>
+
+        {/* Thing 2 */}
+        <div>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default EventItem
