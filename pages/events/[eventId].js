c2a28d65f93e281bc6c91e4bf637fe696eaa1f78
Michael Towson <MTowson@gmail.com>
2021-11-10 15:28:49 -0800

    add event-detail components

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,25 @@
+import { useRouter } from 'next/router';
+import { getEventById} from '../../dummy-data'
+
 function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  
+  //Upon load, check to see if the id entered into the url matches an ID in our data. Store in variable.
+  const event = getEventById(eventId);
+
+
+
+  // --- RENDER ---
+
+  //If an event is not found matching the url, render an error.
+  if (!event) {
+    return (
+      <p>No event by that ID found. Please make a different selection</p>
+    )
+  }
+
+  //...Otherwise, render the page with event details.
   return (
     <div>
       Event details go here
